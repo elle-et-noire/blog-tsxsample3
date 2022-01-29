2aaaaf370e9afc791d3b187114c8f6932d9b1627
L Lawliet <atibon3@gmail.com>
2022-01-29 23:10:36 +0900

    correct

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  basePath: process.env.GITHUB_ACTIONS ? "/blog" : "",
+  basePath: process.env.GITHUB_ACTIONS ? "/blog-tsxsample3" : "",
   reactStrictMode: true,
   trailingSlash: true,
 };
